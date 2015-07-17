32daeadc1d1c7ec44b7bc2f2e1cc8fffb0f66aed
misterhaase <marco_haase@gmx.de>
2015-07-17 10:28:23 +0200

    dev

--- src/main/java/de/amp/amp_server/AmpServer.java
+++ src/main/java/de/amp/amp_server/AmpServer.java
@@ -1,5 +1,7 @@
 package de.amp.amp_server;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -7,26 +9,25 @@ import org.glassfish.jersey.servlet.ServletContainer;
 
 public class AmpServer {
 
+  private static final int PORT = 1337;
+
   public void start() {
     ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
     context.setContextPath("/");
 
     Server jettyServer = new Server(1337);
     jettyServer.setHandler(context);
+    jettyServer.addBean(new AmpErrorHandler());
 
     ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
     jerseyServlet.setInitOrder(0);
-
-    // Tells the Jersey Servlet which REST service/class to load.
-    jerseyServlet.setInitParameter(
-            "jersey.config.server.provider.classnames",
-            RestInterface.class.getCanonicalName());
+    jerseyServlet.setInitParameter("jersey.config.server.provider.classnames", RestInterface.class.getCanonicalName());
 
     try {
       jettyServer.start();
       jettyServer.join();
     } catch (Exception ex) {
-      ex.printStackTrace();
+      Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "fatal error: ", ex);
     } finally {
       jettyServer.destroy();
     }

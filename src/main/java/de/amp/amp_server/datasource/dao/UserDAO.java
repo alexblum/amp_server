b866054aaf456ec8e90668bef2504e42cce3d66b
Aleksej Tsvetkov <sofonja@gmail.com>
2015-07-17 16:36:03 +0200

    add lastLogin update

--- src/main/java/de/amp/amp_server/datasource/dao/UserDAO.java
+++ src/main/java/de/amp/amp_server/datasource/dao/UserDAO.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -39,13 +40,49 @@ public class UserDAO extends AbstractDAO {
     return null;
   }
 
+  public User update(User user) {
+    Connection connection = null;
+    PreparedStatement ps = null;
+    ResultSet resultSet = null;
+
+    try {
+      connection = getConnection();
+      ps = connection.prepareStatement("SELECT * FROM user WHERE id = ?", ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+      ps.setInt(1, user.getId());
+
+      resultSet = ps.executeQuery();
+      if (resultSet.next()) {
+        resultSet.updateBoolean("active", user.isActive());
+        resultSet.updateTimestamp("createDate", new Timestamp(user.getCreateDate().getTime()));
+        resultSet.updateString("email", user.getEmail());
+        resultSet.updateTimestamp("lastLogin", new Timestamp(user.getLastLogin().getTime()));
+        resultSet.updateString("name", user.getName());
+        resultSet.updateString("password", user.getPassword());
+        resultSet.updateRow();
+        return user;
+      }
+
+    } catch (Exception ex) {
+      Logger.getLogger(getClass().getName()).log(Level.WARNING, null, ex);
+    } finally {
+      try {
+        resultSet.close();
+        ps.close();
+        connection.close();
+      } catch (Exception ex) {
+      }
+    }
+
+    return null;
+  }
+
   private User map(ResultSet resultSet) throws SQLException {
     User u = new User();
     u.setId(resultSet.getInt("id"));
     u.setActive(resultSet.getBoolean("active"));
-    u.setCreateDate(resultSet.getDate("createDate"));
+    u.setCreateDate(resultSet.getTimestamp("createDate"));
     u.setEmail(resultSet.getString("email"));
-    u.setLastLogin(resultSet.getDate("lastLogin"));
+    u.setLastLogin(resultSet.getTimestamp("lastLogin"));
     u.setName(resultSet.getString("name"));
     u.setPassword(resultSet.getString("password"));
     return u;
